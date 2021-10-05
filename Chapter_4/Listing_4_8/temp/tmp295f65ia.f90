e7a05bd6447f8d360c9a6ab61170f051b3f5b0fb
Mark Khusid <markkhusid@protonmail.com>
2021-10-05 22:58:14 +0000

    updates to listing 4_8

--- Chapter_4/Listing_4_8/temp/tmp295f65ia.f90
+++ Chapter_4/Listing_4_8/temp/tmp295f65ia.f90
@@ -17,6 +17,10 @@ program tsunami
     integer(int32), parameter :: icenter = 25
     real(real32), parameter   :: decay = 0.02
     
+    logical :: file_exists
+    
+    open(9, file = 'data.txt')
+    
     if (grid_size <= 0) stop 'grid_size must be > 0'
     if (dt <= 0) stop 'time step dt must be > 0'
     if (dx <= 0) stop 'grid spacing dx must be > 0'
@@ -24,9 +28,22 @@ program tsunami
     call set_gaussian(h, icenter, decay)
     
     !print *, 0, h
-    !time_loop: do n = 1, num_time_steps
-        !h = h - c * diff(h) / dx * dt
+    write (9, *) 0, h
+    close(9)
+    
+    time_loop: do n = 1, num_time_steps
+        h = h - c * diff(h) / dx * dt
         !print *, n, h
-    !end do time_loop
+        
+        inquire(file = 'data.txt', exist = file_exists)
+            if (file_exists) then
+                open(9, file = 'data.txt', status = 'old', position = 'append', action = 'write')
+            else
+                open(9, file = 'data.txt', status = "new", action = 'write')
+            end if
+        
+        write (9, *) n, h
+
+    end do time_loop
         
 end program tsunami
